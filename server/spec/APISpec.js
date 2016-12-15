71c6063aec9f93fd6fffbc1257108fa6dc6f7911
Davide <davide95belli@libero.it>
2016-12-15 10:55:15 +0100

    test for deleteStudent added

--- server/spec/APISpec.js
+++ server/spec/APISpec.js
@@ -78,3 +78,67 @@ describe("Test /searchStudent", function() {
 
 
 });
+
+
+//Test for deleteStudent
+describe("Test /deleteStudent", function() {
+	//set the data
+	var data = {ID: '1'};
+	
+	//legal request with ID
+	it("to returns status code 200", function(done) {
+	  client.post(base_url + "deleteStudent/", data, function(err, res, body) {
+		expect(body).toEqual(
+			{
+				ID: "1",
+				SSN: "AB45", 
+				name: "Mattia",
+				address: "via Roma",
+				mark: "5"
+			}
+		);
+
+		done();
+	  });
+	});
+	
+	var data3 = {SSN: 'A6T4'};
+	
+	//legal request with SSN
+	it("to returns status code 200", function(done) {
+	  client.post(base_url + "deleteStudent/", data3, function(err, res, body) {
+		expect(body).toEqual(
+			{
+				ID: "2",
+				SSN: "A6T4",
+				name: "Fabio",
+				address: "via Sommarive",
+				mark: "6"
+			}
+		);
+
+		done();
+	  });
+	});
+	
+	//student non existing
+	data1 = {ID: "10" };
+	it("to returns status code 406", function(done) {
+	  client.post(base_url + "deleteStudent/", data1, function(err, res, body) {
+		expect(res.statusCode).toBe(404);
+		done();
+	  });
+	});
+	
+	//wrong parameter
+	data2 = {name: "1" };
+	it("to returns status code 406", function(done) {
+	  client.post(base_url + "deleteStudent/", data2, function(err, res, body) {
+		expect(res.statusCode).toBe(406);
+		expect(body).toBe(1);
+		done();
+	  });
+	});
+
+
+});
